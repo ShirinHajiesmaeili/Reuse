4afb23e9bac45cf2e3e56a56de4b2fae44121574
Shirin <esshirin486@gmail.com>
2025-02-05 10:22:08 +0100

    backend endpoint

--- Server/routers/cartRouter.js
+++ Server/routers/cartRouter.js
@@ -2,18 +2,18 @@
 import express from "express";
 const router = express.Router();
 import cartController from "../controllers/cartController.js";
-import authenticate from "../middleware/authenticate.js"; // Middleware to ensure the user is authenticated
+// import authenticate from "../middleware/authenticate.js"; // Middleware to ensure the user is authenticated
 
 // Get the current cart
-router.get("/", authenticate, cartController.getCart);
+router.get("/", cartController.getCart);
 
 // Add an item to the cart
-router.post("/add", authenticate, cartController.addToCart);
+router.post("/add", cartController.addToCart);
 
 // Remove an item from the cart
-router.post("/remove", authenticate, cartController.removeFromCart);
+router.post("/remove", cartController.removeFromCart);
 
 // Clear the cart
-router.post("/clear", authenticate, cartController.clearCart);
+router.post("/clear", cartController.clearCart);
 
 export default router;
