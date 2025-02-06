97a17ffe28c6532527df8364242116619f0107ac
Shirin <esshirin486@gmail.com>
2025-02-06 17:03:16 +0100

    navbar fixation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -15,23 +15,25 @@ import SellItems from "./pages/SellItems";
 import ProtectedLayout from "./layout/ProtectedLayout";
 import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp";
+import Profile from "./pages/Profile.jsx";
 import ForgotPassword from "./components/ForgotPassword";
-
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
+    <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} />
-      <Route path='shop-items' element={<ShopItems />} />
-      <Route path='product/:id' element={<Detail />} />
-      <Route path='createpost' element={<ProtectedLayout />}>
-       {/* <Route index element={<CreatePost />} />*/}
+      <Route path="shop-items" element={<ShopItems />} />
+      <Route path="product/:id" element={<Detail />} />
+      <Route path="sell-items" element={<ProtectedLayout />}>
+        <Route index element={<SellItems />} />
       </Route>
-      <Route path='auth' element={<Auth />}>
+      <Route path="profile" element={<ProtectedLayout />}>
+        <Route index element={<Profile />} />
+      </Route>
+      <Route path="auth" element={<Auth />}>
         <Route index element={<SignIn />} />
-        <Route path='signup' element={<SignUp />} />
-        <Route path='forgotpassword' element={<ForgotPassword />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="forgotpassword" element={<ForgotPassword />} />
       </Route>
     </Route>
   )
